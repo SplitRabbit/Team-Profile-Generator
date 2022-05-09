d59e32ef0ff6b97af00fb025051617aac7c51b70
SplitRabbit <jue.kun.liu@gmail.com>
2022-05-09 14:46:03 -0400

    finished line of questioning

--- index.js
+++ index.js
@@ -1,1 +1,101 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager.js');
+const Intern = require('./lib/Intern.js');
+const Engineer = require('./lib/Engineer.js');
+const Employee = require('./lib/Employee.js');
+
+//list of manager questions for inquirer
+const questions = [
+    {
+        type: "list",
+        name: "role",
+        message: "What is the Employees Role?",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their Employee ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email?"
+    },
+    //conditional questions depending on role
+    {
+        type: "input",
+        name: "officenumber",
+        message: "What is their Office Number?",
+        when: (answers) => answers.role === "Manager"
+    },
+    {
+        type: "input",
+        name: "Github",
+        message: "What is their Github Username?",
+        when: (answers) => answers.role === "Engineer"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school do they attend?",
+        when: (answers) => answers.role === "Intern"
+    },
+    //recursive question to reloop through questions 
+    {
+        type: "confirm",
+        name: "addanother",
+        message: "Would you like to add another Employee?"
+    },
+];
+
+//Array for team objects
+let teamArray = [];
+
+//promptuser function
+function promptUser() {
+    console.log("Enter Employee Information Here.");
+    return inquirer.prompt(questions).then((answers) => {
+
+    //conditional for pushing new employee objects into teamArray
+        if (answers.role = "Manager") {
+            const manager = new Manager(answers.name,answers.id,answers.email,answers.officenumber);
+            teamArray.push(Manager);
+        } else if (answers.role = "Engineer") {
+            const engineer = new Engineer(answers.name,answers.id,answers.email,answers.Github);
+            teamArray.push(engineer);
+        } else if (answers.role = "Intern"){
+            const intern = new Intern(answers.name,answers.id,answers.email,answers.school);
+            teamArray.push(intern);
+        };
+    //recursive function to decide whether or not to loop through again
+        if (answers.addanother) {
+            return promptUser();
+        } else {
+            return answers;
+        }
+    });
+};
+
+// TODO: Create a function to initialize app
+async function init() {
+    try {
+    //ask questions
+    const answers =  await promptUser();
+    console.log(teamArray);
+  }   catch(err) {
+    console.log(err);
+  }
+};
+
+
+init();
